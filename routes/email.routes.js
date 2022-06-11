d12ce6e049758303b3f49670b677b321c2ed4375
margri764 <62308899+margri764@users.noreply.github.com>
2022-06-10 21:45:21 -0300

    first commit

---
+++ routes/email.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require ('express');
+const router = Router();
+
+const { email } = require('../controllers/email-hostinger');
+
+
+
+router.post('/',[
+],email); 
+
+
+
+
+
+
+
+module.exports= router;
